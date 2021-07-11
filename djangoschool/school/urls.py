204730ccb0db6761df87decb9d040c35bbdc5a27
7evenights <moonoman007@hotmail.com>
2021-07-11 21:44:02 +0700

    Build Shool Manage app From python
    +frontend (home, register, score)
    +backend (sqlitedatebase)

---
+++ djangoschool/school/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path("", HomePage, name="home-page"),
+    path("about/", AboutPage, name="about-page"),
+    path("contact/", ContactUs, name="contact-page"),
+    path("score/", ShowScore, name="score-page"),
+    path("register/", Register, name="register-page"),
+]
